3b3ad1eade14a893c629459bb9959f294f490b3e
reemmohmed <reemmohmedmohmed@gmail.com>
2024-05-31 21:09:08 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'dart:async';
-
-void main() {
-  runApp(MyApp());
-}
-
-class Employee {
-  final int id;
-  final String name;
-
-  Employee({required this.id, required this.name});
-
-  Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name};
-  }
-}
+import 'package:path/path.dart';
+import 'package:get_it/get_it.dart';
 
 class DatabaseHelper {
-  static final DatabaseHelper _instance = DatabaseHelper.internal();
+  Database? _database;
 
-  factory DatabaseHelper() => _instance;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
 
-  static Database? _db;
-
-  DatabaseHelper.internal();
-
-  Future<Database> get db async {
-    if (_db != null) {
-      return _db!;
-    }
-    _db = await initDb();
-    return _db!;
+    _database = await _initDatabase();
+    return _database!;
   }
 
-  Future<Database> initDb() async {
-    String databasesPath = await getDatabasesPath();
-    String path = join(databasesPath, 'employees.db');
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'your_database_name.db');
 
-    // Delete the database
-    await deleteDatabase(path);
-
-    return await openDatabase(path, version: 1,
-        onCreate: (Database db, int version) async {
-      await db
-          .execute('CREATE TABLE Employee(id INTEGER PRIMARY KEY, name TEXT)');
-    });
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createTables,
+    );
   }
 
-  Future<int> insertEmployee(Employee employee) async {
-    var dbClient = await db;
-    return await dbClient.insert('Employee', employee.toMap());
-  }
+  Future<void> _createTables(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS table1 (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER
+      )
+    ''');
 
-  Future<List<Employee>> getEmployees() async {
-    var dbClient = await db;
-    List<Map> list = await dbClient.rawQuery('SELECT * FROM Employee');
-    List<Employee> employees = [];
-    for (int i = 0; i < list.length; i++) {
-      employees.add(Employee(id: list[i]['id'], name: list[i]['name']));
-    }
-    return employees;
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS table2 (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        description TEXT
+      )
+    ''');
   }
+}
+
+GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerSingleton(DatabaseHelper());
+}
+
+void main() {
+  setupLocator();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    DatabaseHelper databaseHelper = locator<DatabaseHelper>();
+
     return MaterialApp(
-      title: 'Employee Database',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _controller = TextEditingController();
-  List<Employee> _employees = [];
-  bool _isNameValid = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _getEmployees();
-  }
-
-  _getEmployees() async {
-    var dbHelper = DatabaseHelper();
-    List<Employee> employees = await dbHelper.getEmployees();
-    setState(() {
-      _employees = employees;
-    });
-  }
-
-  _addEmployee() async {
-    if (_controller.text.isNotEmpty) {
-      var dbHelper = DatabaseHelper();
-      await dbHelper.insertEmployee(Employee(id: 0, name: _controller.text));
-      _controller.clear();
-      _getEmployees();
-      setState(() {
-        _isNameValid = true;
-      });
-    } else {
-      setState(() {
-        _isNameValid = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Employee Database'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelText: 'Employee Name',
-                errorText: _isNameValid ? null : 'Please enter a valid name',
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: _addEmployee,
-            child: Text('Add Employee'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _employees.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(_employees[index].name),
-                );
-              },
-            ),
-          ),
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
   }
